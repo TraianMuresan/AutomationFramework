38f479a71ef8982e0c59bcf3581960906754bccd
TraianMuresan <mtraian75@yahoo.com>
2022-11-09 18:14:34 +0200

    fix driver

--- src/test/java/Share/ShareData.java
+++ src/test/java/Share/ShareData.java
@@ -2,6 +2,7 @@ package Share;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import java.time.Duration;
@@ -11,7 +12,13 @@ public class ShareData {
 
     @BeforeMethod
     public void preparedriver(){
-        driver = new ChromeDriver();
+
+        Boolean cicd=Boolean.parseBoolean(System.getProperty("cicd"));
+        ChromeOptions chromeOptions=new ChromeOptions();
+        if(cicd){
+            chromeOptions.addArguments("--headless");
+        }
+        driver = new ChromeDriver(chromeOptions);
         driver.get("https://demo.automationtesting.in/Index.html");
         driver.manage().window().maximize();
 
